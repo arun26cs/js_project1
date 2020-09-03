1fc1478d67b239958587d91e3cac9d07ac117c9d
arun varghese <arun26cs@gmail.com>
2020-09-03 17:38:29 -0400

    dynamic change of the indication, background all based on hour is implemented

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ var content = document.getElementById("content");
 var nameselector = document.getElementById("name");
 
 
-
+/**
+ * Function: formatTime()
+ * Trigger: this is triggered every seconf and the time is updated every second
+ * setTimeout(formatTime, 1000);
+ * has to update the code to create an observer for the image to load only when the time to   change reaches
+ */
 function formatTime() {
     let date = new Date();
     //console.log(date.getDate());
@@ -54,18 +59,56 @@ function addZero(input) {
 
 function formatDay() {
     let date = new Date();
-    if (date.getHours() < 12) {
+    if (date.getHours() < 12 && date.getHours() > 4) {
         greetselector.innerHTML = "Good Morning";
-    } else if (date.getHours() < 18) {
+    } else if (date.getHours() > 12 && date.getHours() < 18) {
         greetselector.innerHTML = "Good afternoon";
     } else {
         greetselector.innerHTML = "Good Eveneing"
     }
+    setBg();
 }
 
-function setName() {
-    if (localStorage.getItem('name') === null) {
-        nameselector.innerHTML = '[Enter name]';
+/*
+
+    Function: setName
+    Triggered when the name tag is clicked and we start to type and save it by taking the pointer away. no need to call this function the event is constantly listening and calls when its fired.
+    even added: blur() and keypress()
+    storage: it is stored in the localstorage with the param name "name"
+    the parameter for the function: it has the details of the box we are editing all the key we press all the data we type inclusive
+
+*/
+function setName(keyevent) {
+    if (keyevent === 'keypress') {
+        if (keyevent.which == 13 || keyevent.keyCode == 13) {
+            localStorage.setItem("name", keyevent.target.innerText);
+            nameselector.blur();
+        }
+    } else {
+        localStorage.setItem("name", keyevent.target.innerText);
+    }
+}
+
+/**
+ * 
+ * Function: setBg(setting the background)
+ * no even listeners are added
+ * function trigger: when the hour is >4 and <12 then its morning so the morning image is added
+ * when the hour is >12 and <18 then the afternoon image is added
+ * when the hour is >18 and <4AM then the imaged added is night
+ */
+function setBg() {
+    let date = new Date();
+    if (date.getHours() < 12 && date.getHours() > 4) {
+        document.body.style.backgroundImage = "url('/image/morning.jpg')";
+        document.body.style.backgroundSize = 'fixed';
+    } else if (date.getHours() > 12 && date.getHours() < 17) {
+        document.body.style.backgroundImage = "url('/image/afternoon.jpg')";
+        document.body.style.backgroundSize = 'fixed';
+    } else {
+        document.body.style.backgroundImage = "url('/image/night.jpg')";
+        document.body.style.backgroundSize = 'fixed';
+        document.body.style.color = 'white';
     }
 }
 
